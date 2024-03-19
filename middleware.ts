ff1a26e723a01742c29d6e4a472d9486d84c39fb
dark9wesley <pengyw97@gmail.com>
2024-03-19 16:31:05 +0800

    feat: 移除ip限制

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { authMiddleware } from '@clerk/nextjs'
-import { get } from '@vercel/edge-config'
 import { type NextRequest, NextResponse } from 'next/server'
 
 import { kvKeys } from '~/config/kv'
@@ -15,21 +14,8 @@ export const config = {
 async function beforeAuthMiddleware(req: NextRequest) {
   const { geo, nextUrl } = req
 
-  const blockedIPs = await get<string[]>('blocked_ips')
   const ip = getIP(req)
   const isApi = nextUrl.pathname.startsWith('/api/')
-
-  if (blockedIPs?.includes(ip)) {
-    if (isApi) {
-      return NextResponse.json(
-        { error: 'You have been blocked.' },
-        { status: 403 }
-      )
-    }
-
-    nextUrl.pathname = '/blocked'
-    return NextResponse.rewrite(nextUrl)
-  }
 
   if (nextUrl.pathname === '/blocked') {
     nextUrl.pathname = '/'
